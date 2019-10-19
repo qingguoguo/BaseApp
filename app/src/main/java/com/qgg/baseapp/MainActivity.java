a52592ea9e591e9b511e1b6585c2c71d93a4f529
wangqing <wangqing@ximalayaos.com>
2019-10-19 12:23:40 +0800

    feat：增加tools module

--- app/src/main/java/com/qgg/baseapp/MainActivity.java
+++ app/src/main/java/com/qgg/baseapp/MainActivity.java
@@ -4,16 +4,17 @@ import android.animation.ValueAnimator;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import com.qgg.jni.HelloLibrary;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.tv);
         textView.setText("显示动画啊");
         //method1(textView);
-        method2(textView, "哈哈，你看我可以移动的");
-        method2(textView, new HelloLibrary().sayHello());
+//        method2(textView, "哈哈，你看我可以移动的");
+//        method2(textView, new HelloLibrary().sayHello());
+        getDensity();
     }
 
     private void method2(final TextView textView, final String text) {
@@ -45,6 +47,29 @@ public class MainActivity extends AppCompatActivity {
         });
         valueAnimator.setDuration(text.length() * 150 + 200);
         valueAnimator.start();
+    }
+
+    private void getDensity() {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+
+        Log.e(TAG, "Density is " + displayMetrics.density + " densityDpi is " + displayMetrics.densityDpi);
+        Log.e(TAG, "屏幕分辨率：" + displayMetrics.heightPixels + "*" + displayMetrics.widthPixels + "px（height*width）");
+        Log.e(TAG, "屏幕物理尺寸：" + displayMetrics.heightPixels + "*" + displayMetrics.widthPixels + "英寸");
+
+        Log.e(TAG, "ppi：每一英寸所对应的像素数目");
+        Log.e(TAG, "Density-independent pixel (dp)独立像素密度。标准是160dip.即1dp对应1个pixel，计算公式如：px = dp * (dpi / 160)，屏幕密度越大，1dp对应 的像素点越多。\n" +
+                "上面的公式中有个dpi，dpi为DPI是Dots Per Inch（每英寸所打印的点数），也就是当设备的dpi为160的时候1px=1dp");
+        getScreenSizeOfDevice2();
+    }
+
+    private void getScreenSizeOfDevice2() {
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getRealSize(point);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        double x = Math.pow(point.x / dm.xdpi, 2);
+        double y = Math.pow(point.y / dm.ydpi, 2);
+        double screenInches = Math.sqrt(x + y);
+        Log.e(TAG, "Screen inches : " + screenInches);
     }
 
     class TestBroadcastReceiver extends BroadcastReceiver {
