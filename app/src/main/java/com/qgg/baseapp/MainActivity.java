4f9d2601ccc115159af8a57e3350aeaf03c6858f
wangqing <wangqing@ximalayaos.com>
2019-10-21 15:08:59 +0800

    feat：添加ScreenUtil工具类

--- app/src/main/java/com/qgg/baseapp/MainActivity.java
+++ app/src/main/java/com/qgg/baseapp/MainActivity.java
@@ -1,17 +1,12 @@
 package com.qgg.baseapp;
 
-import android.animation.ValueAnimator;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.qgg.tools.ToolsActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -20,62 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // TestBroadcastReceiver testBroadcastReceiver = new TestBroadcastReceiver();
-        // getApplicationContext().registerReceiver(testBroadcastReceiver, );
-        // unregisterReceiver(testBroadcastReceiver);
     }
 
-    public void startAnimator(View view) {
-        TextView textView = findViewById(R.id.tv);
-        textView.setText("显示动画啊");
-        //method1(textView);
-//        method2(textView, "哈哈，你看我可以移动的");
-//        method2(textView, new HelloLibrary().sayHello());
-        getDensity();
+    /**
+     * 创建菜单
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
-    private void method2(final TextView textView, final String text) {
-        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, text.length());
-        valueAnimator.setInterpolator(new FastOutSlowInInterpolator());
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                int index = (int) animation.getAnimatedValue();
-                Log.e("MainActivity", "index = " + index);
-                textView.setText(text.substring(0, index));
-            }
-        });
-        valueAnimator.setDuration(text.length() * 150 + 200);
-        valueAnimator.start();
-    }
-
-    private void getDensity() {
-        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-
-        Log.e(TAG, "Density is " + displayMetrics.density + " densityDpi is " + displayMetrics.densityDpi);
-        Log.e(TAG, "屏幕分辨率：" + displayMetrics.heightPixels + "*" + displayMetrics.widthPixels + "px（height*width）");
-        Log.e(TAG, "屏幕物理尺寸：" + displayMetrics.heightPixels + "*" + displayMetrics.widthPixels + "英寸");
-
-        Log.e(TAG, "ppi：每一英寸所对应的像素数目");
-        Log.e(TAG, "Density-independent pixel (dp)独立像素密度。标准是160dip.即1dp对应1个pixel，计算公式如：px = dp * (dpi / 160)，屏幕密度越大，1dp对应 的像素点越多。\n" +
-                "上面的公式中有个dpi，dpi为DPI是Dots Per Inch（每英寸所打印的点数），也就是当设备的dpi为160的时候1px=1dp");
-        getScreenSizeOfDevice2();
-    }
-
-    private void getScreenSizeOfDevice2() {
-        Point point = new Point();
-        getWindowManager().getDefaultDisplay().getRealSize(point);
-        DisplayMetrics dm = getResources().getDisplayMetrics();
-        double x = Math.pow(point.x / dm.xdpi, 2);
-        double y = Math.pow(point.y / dm.ydpi, 2);
-        double screenInches = Math.sqrt(x + y);
-        Log.e(TAG, "Screen inches : " + screenInches);
-    }
-
-    class TestBroadcastReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
+    /**
+     * 菜单的点击事件
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_tools:
+                startActivity(new Intent(this, ToolsActivity.class));
+                break;
+            default:
+                break;
         }
+        return true;
     }
 }
--- tools/src/main/java/com/qgg/tools/ToolsActivity.java
+++ tools/src/main/java/com/qgg/tools/ToolsActivity.java
@@ -1,13 +1,46 @@
 package com.qgg.tools;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 public class ToolsActivity extends AppCompatActivity {
+    private static final String TAG = "ToolsActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tools);
+
+        getScreenParameters();
+    }
+
+    private void getScreenParameters() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Density = ").append(ScreenUtil.getDensity()).append("，densityDpi = ").append(ScreenUtil.getDensityDpi()).append(System.getProperty("line.separator"));
+        stringBuilder.append("屏幕分辨率 = ").append(ScreenUtil.getScreenWidth()).append("*").append(ScreenUtil.getScreenHeight()).append("px（width*height）").append(System.getProperty("line.separator"));
+        stringBuilder.append("屏幕物理尺寸 = ").append(ScreenUtil.getScreenSizeInches()).append(" 英寸").append(System.getProperty("line.separator"));
+        stringBuilder.append("1dp = ").append(ScreenUtil.getDensity()).append(" px").append(System.getProperty("line.separator"));
+        stringBuilder.append("精确密度 = ").append(ScreenUtil.getScreenWidth() / ScreenUtil.getDensity()).append("*").
+                append(ScreenUtil.getScreenHeight() / ScreenUtil.getDensity()).append("dp（width*height）").append(System.getProperty("line.separator"));
+
+        stringBuilder.append(System.getProperty("line.separator"));
+        stringBuilder.append(System.getProperty("line.separator"));
+        stringBuilder.append(System.getProperty("line.separator"));
+        stringBuilder.append(System.getProperty("line.separator"));
+        stringBuilder.append(System.getProperty("line.separator"));
+
+        stringBuilder.append("ppi：每一英寸所对应的像素数目").append(System.getProperty("line.separator")).append(System.getProperty("line.separator"));
+        stringBuilder.append("dp或者dip ：独立像素密度（Density-independent pixel）。").append("标准是160dip，即1dp对应1个pixel，计算公式如：px = dp * (dpi / 160)，屏幕密度越大，1dp对应 的像素点越多。")
+                .append("上面的公式中有个dpi，也就是当设备的dpi为160的时候1px=1dp").append(System.getProperty("line.separator")).append(System.getProperty("line.separator"));
+        stringBuilder.append("dpi：DPI是Dots Per Inch（每英寸所打印的点数）,像素密度，指的是在系统软件上指定的单位尺寸的像素数量，它往往是写在系统出厂配置文件的一个固定值");
+
+        TextView textView = findViewById(R.id.hint);
+        textView.setText(stringBuilder.toString());
+
+        Log.e(TAG, stringBuilder.toString());
     }
 }
+
